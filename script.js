1355450b8f726e919b7d873896ca1b944e464f86
LeZuni <leslymendez896@gmail.com>
2026-06-08 15:41:39 -0600

    feat: :sparkles: add mostrar menu

---
+++ script.js
@@ -0,0 +1,18 @@
+let habitaciones = [];
+
+function mostrarMenu() {
+  let opcion = prompt(
+    "=== HOTEL ===\n 1. Registrar nueva habitación\n 2. Listar habitaciones\n 3. Buscar habitación por número\n 4. Cambiar estado de una habitación\n 5. Eliminar habitación\n 6. Salir\n\n Elige una opción:",
+  );
+  if (opcion === "1") registrarHabitacion();
+  else if (opcion === "2") listarHabitaciones();
+  else if (opcion === "3") buscarHabitacion();
+  else if (opcion === "4") cambiarEstado();
+  else if (opcion === "5") eliminarHabitacion();
+  else if (opcion === "6") {
+    console.log("Saliendo del sistema. ¡Hasta luego!");
+  } else {
+    console.log("Opción no válida. Inténtalo de nuevo.");
+    mostrarMenu();
+  }
+}
